c99d95bacfcdcce70204f6e274b45078d6648a5f
KKdohyeong <dohyeong254@naver.com>
2024-09-14 14:51:18 +0900

    feat : 우승자 및 게임 완성

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -2,8 +2,30 @@ package racingcar.model;
 
 public class Car {
     private String name;
-
+    private int position;
     Car(String msg){
-
+        this.name = msg;
+        position = 0;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void movePosition(){
+        position++;
+    }
+
+    public int getPositionInt(){
+        return position;
+    }
+    public String getPosition() {
+        StringBuilder positionStr = new StringBuilder(); // StringBuilder로 변경
+        for(int i = 0; i < position; i++) {
+            positionStr.append("-"); // append 메서드 사용
+        }
+        return positionStr.toString(); // 문자열로 변환하여 반환
+    }
+
+
 }
--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,5 +1,7 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -25,5 +27,33 @@ public class Cars {
             Car car = new Car(msg.trim());
             cars.add(car);
         }
+    }
+
+    public void moveCars(){
+        for(Car car : cars){
+            if(Randoms.pickNumberInRange(0,9) >=4){
+                car.movePosition();
+            }
+        }
+    }
+
+    public List<Car> getCars(){
+        return  cars;
+    }
+
+    public List<Car> getWinner(){
+        int max = 0;
+        for(Car car: cars){
+            if(car.getPositionInt() > max){
+                max = car.getPositionInt();
+            }
+        }
+        List<Car> winnerCars = new ArrayList<>();
+        for(Car car: cars){
+            if(car.getPositionInt() ==max){
+                winnerCars.add(car);
+            }
+        }
+        return winnerCars;
     }
 }
